ea0350957928fb99654bbfa0f2668c65780fabc8
Saad Ahmed <asadnwfp@gmail.com>
2015-11-30 13:27:06 +0500

    Layout Added to MainActivity

---
+++ app/src/main/java/com/picodealpha/marshmallowpermissions/MainActivity.java
@@ -0,0 +1,21 @@
+package com.picodealpha.marshmallowpermissions;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *  @author  Saad Ahmed
+ *  @version 1
+ *  This project has been implementd on the working of the following link.
+ *
+ *  http://code.tutsplus.com/articles/understanding-permissions-in-android-m--cms-24443
+ *  Reffer to the above link for further info.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
